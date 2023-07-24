72fd675420effa5fa436d30e8a9175a3dd098a0b
Fernando Robles <fernandorobles97@yahoo.com>
2023-07-24 13:01:32 -0500

    Test what appears in app on load, add fixtures file, stub fetch requests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('User flows', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+    cy.intercept('GET', 'http://localhost:3001/api/v1/tricks', {
+      statusCode: 200,
+      fixture: 'tricks'
+    })
+  })
+  it('Should include a title, form, 3 tricks', () => {
+    cy.get('h1').contains('Sick Trick Wish List')
   })
 })
